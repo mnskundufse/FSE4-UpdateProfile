2175a09fece831155805e75bcfca4ffb18e2832f
mnskundu <mnskundu.fse@gmail.com>
2022-06-01 19:57:43 +0530

    Test Project Initial Checkin

---
+++ Engineer.UpdateProfileService/Engineer.UpdateProfileService.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Engineer.UpdateProfileService.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
